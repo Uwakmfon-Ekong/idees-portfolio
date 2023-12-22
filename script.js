9f17d8e96fbd6aa42106f140f3da2899d192cc68
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2023-12-22 15:55:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipletext", {
+  strings: ["18 years old", "data analyst", "social media marketer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backdelay: 1000,
+  loop: true,
+});
