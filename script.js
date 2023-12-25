ddc267f53c588dc02d4219c28b00269dc400c8d7
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2023-12-25 11:02:24 +0100

    added javascript

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const menus = document.querySelector(".navList ul");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click",() => {
+  menus.classList.toggle("display")
+})
+closeBtn.addEventListener("click", () => {
+  menus.classList.remove("display");
+});
+
+
 var typed = new Typed(".multipletext", {
   strings: ["18 years old", "data analyst", "social media marketer"],
   typeSpeed: 50,
